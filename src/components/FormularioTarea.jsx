4efbed3d3c9d0a53d8823ebc43fbf7693c454b78
Ignacio Ramon <joseignacioramon24@gmail.com>
2025-03-25 19:43:56 -0300

    agregamos las tareas validadas al estado listaTareas

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,19 +1,56 @@
 import { Button, Form } from "react-bootstrap";
 import { PlusCircle } from "react-bootstrap-icons";
 import ListaTarea from "./ListaTarea";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 const FormularioTarea = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+  const [listaTareas, setListaTareas] = useState([]);
+
+  const posteriorValidacion = (data) => {
+    if (listaTareas.some((tarea) => tarea === data.tarea.trim())) {
+      alert("Esta tarea ya existe");
+    } else {
+      setListaTareas([...listaTareas, data.tarea]);
+      reset();
+    }
+  };
+
   return (
     <section className="mb-4">
-      <Form className="container d-flex">
+      <Form
+        onSubmit={handleSubmit(posteriorValidacion)}
+        className="container d-flex"
+      >
         <Form.Group controlId="formTarea" className="w-100">
-          <Form.Control type="text" placeholder="Ingrese una tarea" />
+          <Form.Control
+            type="text"
+            placeholder="Ingrese una tarea"
+            {...register("tarea", {
+              required: "La tarea es un dato obligatorio",
+              minLength: {
+                value: 3,
+                message: "La tarea debe tener 3 caracteres como mínimo",
+              },
+              maxLength: {
+                value: 25,
+                message: "La tarea debe tener 25 caracteres como máximo",
+              },
+            })}
+          />
+          <Form.Text className="text-danger">{errors.tarea?.message}</Form.Text>
         </Form.Group>
         <Button variant="primary" type="submit">
           <PlusCircle></PlusCircle>
         </Button>
       </Form>
-      <ListaTarea  />
+      <ListaTarea />
     </section>
   );
 };
